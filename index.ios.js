ecd6704810d1b35930ce67514d9837f2a73c4840
Mate Papp <matepapp@icloud.com>
2017-07-27 15:33:56 +0200

    Separated App logic to one component.

--- index.ios.js
+++ index.ios.js
@@ -3,42 +3,7 @@
  * https://github.com/facebook/react-native
  * @flow
  */
+import { AppRegistry } from "react-native";
+import App from "./src/App";
 
-import React, { Component } from "react";
-import { AppRegistry, StyleSheet, Text, View } from "react-native";
-
-export default class ReactNativeCalculator extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started</Text>
-        <Text style={styles.instructions}>Hello World!</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#BBBBBB"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
-});
-
-AppRegistry.registerComponent(
-  "ReactNativeCalculator",
-  () => ReactNativeCalculator
-);
+AppRegistry.registerComponent("ReactNativeCalculator", () => App);
