6f3707e124d37bb3b045c9e2e50049a2556e5cf7
Mate Papp <matepapp@icloud.com>
2017-07-27 12:25:39 +0200

    Added Prettier.

--- index.ios.js
+++ index.ios.js
@@ -5,23 +5,16 @@
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
 
 export default class ReactNativeCalculator extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit index.ios.js</Text>
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
